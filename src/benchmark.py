2487606bd9986ad39a00a9607c5d7cbec43f98d8
Benjamin Lefaudeux <ben@photoroom.com>
2024-09-25 14:10:54 +0000

    Better readme, some more demo code

---
+++ src/benchmark.py
@@ -0,0 +1,80 @@
+from datago import datago  # type: ignore
+import time
+import typer
+from tqdm import tqdm
+import numpy as np
+from polyglot import go_array_to_pil_image, go_array_to_numpy
+
+
+def benchmark(
+    source: str = typer.Option("SOURCE", help="The source to test out"),
+    limit: int = typer.Option(2000, help="The number of samples to test on"),
+    crop_and_resize: bool = typer.Option(True, help="Crop and resize the images on the fly"),
+    require_images: bool = typer.Option(True, help="Request the original images"),
+    require_embeddings: bool = typer.Option(False, help="Request embeddings"),
+    test_masks: bool = typer.Option(True, help="Test masks"),
+    test_latents: bool = typer.Option(True, help="Test latents"),
+):
+    print(f"Running benchmark for {source} - {limit} samples")
+    client = datago.GetClient(
+        source=source,
+        require_images=require_images,
+        require_embeddings=require_embeddings,
+        has_attributes="",
+        lacks_attributes="",
+        has_masks="segmentation_mask" if test_masks else "",
+        lacks_masks="",
+        has_latents="masked_image,my_test_latents" if test_latents else "",
+        lacks_latents="",
+        crop_and_resize=crop_and_resize,
+        prefetch_buffer_size=256,
+        samples_buffer_size=256,
+        downloads_concurrency=64,
+    )
+    client.Start()
+    start = time.time()
+
+    # Make sure in the following that we compare apples to apples, meaning in that case
+    # that we materialize the payloads in the python scope in the expected format
+    # (PIL.Image for images and masks for instance, numpy arrays for latents)
+    img, mask, masked_image = None, None, None
+    for _ in tqdm(range(limit), dynamic_ncols=True):
+        sample = client.GetSample()
+        if sample.ID:
+            # Bring the masks and image to PIL
+            if hasattr(sample, "Image"):
+                img = go_array_to_pil_image(sample.Image)
+
+            if hasattr(sample, "Masks"):
+                for _, mask_buffer in sample.Masks.items():
+                    mask = go_array_to_pil_image(mask_buffer)
+
+            if hasattr(sample, "AdditionalImages") and "masked_image" in sample.AdditionalImages:
+                masked_image = go_array_to_pil_image(sample.AdditionalImages["masked_image"])
+
+            # Bring the latents to numpy
+            if hasattr(sample, "Latents"):
+                for _, latent_buffer in sample.Latents.items():
+                    _latents = go_array_to_numpy(latent_buffer)
+
+            # Bring the embeddings to numpy
+            if hasattr(sample, "CocaEmbedding"):
+                _embedding = np.array(sample.CocaEmbedding)
+
+    fps = limit / (time.time() - start)
+    print(f"FPS {fps:.2f}")
+    client.Stop()
+
+    # Save the last image as a test
+    if img is not None:
+        img.save("benchmark_last_image.png")
+
+    if mask is not None:
+        mask.save("benchmark_last_mask.png")
+
+    if masked_image is not None:
+        masked_image.save("benchmark_last_masked_image.png")
+
+
+if __name__ == "__main__":
+    typer.run(benchmark)
---
+++ src/polyglot.py
@@ -0,0 +1,66 @@
+import ctypes
+from PIL import Image
+import io
+from typing import Optional
+import numpy as np
+
+
+def uint8_array_to_numpy(go_array):
+    if go_array.DataPtr == 0:
+        print("Error: null pointer")
+        return np.array([], dtype=np.uint8)
+
+    # By convention, arrays which are already serialized as jpg or png are not reshaped
+    # We export them from Go with a Channels dimension of -1 to mark them as dimensionless.
+    # Anything else is a valid number of channels and will thus lead to a reshape
+    num_final_channels = max(go_array.Channels, 1)
+    length = go_array.Width * go_array.Height * num_final_channels if go_array.Channels > 0 else len(go_array.Data)
+    shape = (go_array.Height, go_array.Width, go_array.Channels) if go_array.Channels > 0 else (length,)
+
+    # Wrap the buffer around to create a numpy array. Strangely, shape needs to be passed twice
+    # This is a zero-copy operation
+    return np.ctypeslib.as_array((ctypes.c_uint8 * length).from_address(go_array.DataPtr), shape=shape).reshape(shape)
+
+
+def go_array_to_numpy(go_array) -> Optional[np.ndarray]:
+    # Generic numpy-serialized array
+
+    if go_array.DataPtr == 0:
+        # No data in the buffer
+        return None
+
+    np_bytes = np.ctypeslib.as_array(
+        (ctypes.c_uint8 * go_array.Len).from_address(go_array.DataPtr), shape=(go_array.Len,)
+    )
+    bytes_io = io.BytesIO(np_bytes.tobytes())
+    try:
+        return np.load(bytes_io, allow_pickle=False)
+    except ValueError:
+        # Do not try to handle these, return None and we'll handle it in the caller
+        print("Could not deserialize numpy array")
+        return None
+
+
+def go_array_to_pil_image(go_array):
+    # Zero copy conversion of the image buffer from Go to PIL.Image
+
+    if go_array.DataPtr == 0:
+        # No data in the image buffer
+        return None
+
+    np_array = uint8_array_to_numpy(go_array)
+    if go_array.Channels <= 0:
+        # Do not try to decode, we have a jpg or png buffer already
+        return np_array
+
+    h, w, c = np_array.shape
+
+    # Greyscale image
+    if c == 1:
+        return Image.fromarray(np_array[:, :, 0], mode="L")
+
+    if c == 4:
+        return Image.frombuffer("RGBA", (w, h), np_array, "raw", "RGBA", 0, 1)
+
+    assert c == 3, "Expected 3 channels"
+    return Image.fromarray(np_array)
