3c7243002cadf608d4c84ff395eee211d07ebb29
Benjamin Lefaudeux <ben@photoroom.com>
2025-03-10 15:21:16 +0000

    aligning with internal workloads

--- src/worker_http.rs
+++ src/worker_http.rs
@@ -331,7 +331,7 @@ async fn async_pull_samples(
     println!("http_worker: total samples sent: {}\n", count);
 
     // Signal the end of the stream
-    samples_tx.send(None).unwrap();
+    if samples_tx.send(None).is_ok() {} // Channel could have been closed by a .stop() call
 }
 
 pub fn pull_samples(
